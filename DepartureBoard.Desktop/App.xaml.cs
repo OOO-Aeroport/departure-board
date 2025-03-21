2a28cbcc9674d9eb7dc9bc300e4a865b7234fc89
maratmarat22 <144519273+maratmarat22@users.noreply.github.com>
2025-03-21 22:56:47 +0300

    minor 2

--- DepartureBoard.Desktop/App.xaml.cs
+++ DepartureBoard.Desktop/App.xaml.cs
@@ -1,14 +1,9 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Net.Http;
-using System.Windows;
-
-namespace DepartureBoard.Desktop;
+﻿namespace DepartureBoard.Desktop;
 
 /// <summary>
 /// Interaction logic for App.xaml
 /// </summary>
-public partial class App : Application
+public partial class App
 {
     public App()
     {
