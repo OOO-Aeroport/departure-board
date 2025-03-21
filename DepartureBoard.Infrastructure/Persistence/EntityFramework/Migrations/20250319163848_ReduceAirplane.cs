3488d6474847c03e284e22ac7f7f33ed6f680105
maratmarat22 <144519273+maratmarat22@users.noreply.github.com>
2025-03-21 22:28:25 +0300

    some structural changes

---
+++ DepartureBoard.Infrastructure/Persistence/EntityFramework/Migrations/20250319163848_ReduceAirplane.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DepartureBoard.Infrastructure.Persistence.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReduceAirplane : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CurrentFuel",
+                table: "Airplanes");
+
+            migrationBuilder.DropColumn(
+                name: "MaxFuel",
+                table: "Airplanes");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CurrentFuel",
+                table: "Airplanes",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MaxFuel",
+                table: "Airplanes",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
